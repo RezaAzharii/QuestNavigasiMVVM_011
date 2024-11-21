1dd1fe522d5ea19c01e0bbac5b0fb32601070d01
RezaAzharii <rezaazhari867@gmail.com>
2024-11-21 18:41:06 +0700

    Menambahkan scaffold dan NavHost serta menetapkan startDestination pada Halaman.Form

--- app/src/main/java/com/example/viewmodel/Navigations.kt
+++ app/src/main/java/com/example/viewmodel/Navigations.kt
@@ -1,9 +1,13 @@
 package com.example.viewmodel
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.viewmodel.ui.viewmodel.MahasiswaViewModel
 
@@ -18,5 +22,13 @@ fun Navigations(
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
 ){
+    Scaffold { innerPadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+        NavHost(
+            modifier = modifier.padding(innerPadding),
+            navController = navHost, startDestination = Halaman.Form.name
+        ){
 
+        }
+    }
 }
