769de8c6c71cf2ce5342cf00646202640f09c4d6
RezaAzharii <rezaazhari867@gmail.com>
2024-11-21 18:32:45 +0700

    mengubah nama objet dan variabel

--- app/src/main/java/com/example/viewmodel/model/ListJK.kt
+++ app/src/main/java/com/example/viewmodel/model/ListJK.kt
@@ -2,8 +2,8 @@ package com.example.viewmodel.model
 
 import com.example.viewmodel.R
 
-object ListGender {
-    val listGender = listOf(
+object ListJK {
+    val listJk = listOf(
         R.string.laki,
         R.string.wanita
     )
